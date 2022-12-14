3357a441508c1770011542b2647b4ab78231309f
Kail <116641179+KailLow@users.noreply.github.com>
2022-12-14 15:45:13 +0700

    add LoginScreen

---
+++ Roga/LoginScreen.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Roga
+{
+    public partial class LoginScreen : Form
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+
+            string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string sFile = System.IO.Path.Combine(sCurrentDirectory, @"..\..\..\Roga\Assets\Images\LoginScreen.jpg");
+            string sFilePath = Path.GetFullPath(sFile);
+            pbBackground.Image = Image.FromFile(sFilePath);
+
+            sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            sFile = System.IO.Path.Combine(sCurrentDirectory, @"..\..\..\Roga\Assets\Images\logo.png");
+            sFilePath = Path.GetFullPath(sFile);
+            pbLogo.Image = Image.FromFile(sFilePath);
+        }
+
+        private void txtUsername_Enter(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "Please Enter Your Name")
+            {
+                txtUsername.Text = "";
+                txtUsername.ForeColor = SystemColors.WindowText;
+            }
+        }
+
+        private void txtUsername_Leave(object sender, EventArgs e)
+        {
+            if (txtUsername.Text.Length == 0)
+            {
+                txtUsername.Text = "Please Enter Your Name";
+                txtUsername.ForeColor = SystemColors.GrayText;
+            }
+        }
+    }
+}
