f6b8aa0af304c72b76df3312b25673ea58b720f7
LAPTOP-S74F9L51\Ideapad gaming 3 <univerbachtuoc@gmail.com>
2022-12-14 15:55:42 +0700

    Add: Mainscreen (no icon, test) Add: Picture Panel

--- Roga/Program.cs
+++ Roga/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Roga
 {
@@ -10,6 +11,8 @@ namespace Roga
     {
         static void Main(string[] args)
         {
+            MainScreen x = new MainScreen();
+            Application.Run(x);
         }
     }
 }
