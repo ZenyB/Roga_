cac4b2fd53d8b2c9cd83369405d0549933a70f45
Kail <116641179+KailLow@users.noreply.github.com>
2023-01-18 22:24:17 +0700

    add Text feature

---
+++ Roga/CustomTextbox.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Roga
+{
+    public partial class CustomTextbox : RichTextBox
+    {
+        public CustomTextbox()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            BackColor = Color.Transparent;
+
+            ForeColor = Color.Black;
+        }
+
+        public sealed override Color BackColor
+        {
+            get => base.BackColor;
+            set => base.BackColor = value;
+        }
+
+        public CustomTextbox(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
