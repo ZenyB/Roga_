aa1ca265ae8efac740c4828c92a05e70b912a6e6
ZenyB <106144398+ZenyB@users.noreply.github.com>
2023-02-09 05:30:50 +0700

    Update USER_.cs

--- Roga/USER_.cs
+++ Roga/USER_.cs
@@ -20,7 +20,7 @@ namespace Roga
             this.IMAGE_ = new HashSet<IMAGE_>();
         }
     
-        public int ID { get; }
+        public int id { get; set; }
         public string username { get; set; }
         public string password { get; set; }
         public string fullname { get; set; }
